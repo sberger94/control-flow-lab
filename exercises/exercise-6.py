2f0b4f27e45ecd8a6e514a3df46f26486aac9f3d
sberger94 <sberger94@gmail.com>
2022-04-19 16:13:12 -0700

    Excercise 6 complete

--- exercises/exercise-6.py
+++ exercises/exercise-6.py
@@ -19,3 +19,38 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+
+input_month = input("Enter the month of the year (Jan - Dec): ")
+months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+months_31 = ['jan', 'mar', 'may', 'jul', 'aug', 'oct', 'dec']
+months_30 = ['apr', 'jun', 'sep', 'nov']
+
+while input_month.lower() not in months:
+    print("I don't recognize that month!")
+    input_month = input("Enter the month of the year (Jan - Dec): ")
+
+input_day = int(input("Enter the day of the month: "))
+
+if input_month.lower() in months_31:   
+    while input_day not in range(1, 32):
+        print("Please choose a valid day")
+        input_day = int(input("Enter the day of the month: "))
+elif input_month.lower() in months_30:
+    while input_day not in range(1, 31):
+        print("Please choose a valid day")
+        input_day = int(input("Enter the day of the month: "))
+else:
+    while input_day not in range(1, 30):
+        print("Please choose a valid day")
+        input_day = int(input("Enter the day of the month: "))
+
+if (input_month.lower() == 'dec' and input_day >= 21) or input_month.lower() in ['jan', 'feb'] or (input_month.lower() == 'mar' and input_day <= 19):
+    season = 'Winter'
+elif (input_month.lower() == 'mar' and input_day >= 20) or input_month.lower() in ['apr', 'may'] or (input_month.lower() == 'jun' and input_day <= 20):
+    season = 'Spring'
+elif (input_month.lower() == 'jun' and input_day >= 21) or input_month.lower() in ['jul', 'aug'] or (input_month.lower() == 'sep' and input_day <= 21):
+    season = 'Summer'
+else:
+    season = 'Fall'
+
+print(f"{input_month.capitalize()} {input_day} is in {season}")
