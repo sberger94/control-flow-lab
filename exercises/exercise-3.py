30da1a494431908ed31d5eb82a84bc166eff2f88
sberger94 <sberger94@gmail.com>
2022-04-19 13:49:51 -0700

    Exercise 3 complete

--- exercises/exercise-3.py
+++ exercises/exercise-3.py
@@ -12,4 +12,10 @@
 # Hint:  Use the int() function to convert the string returned from input() into an integer
 
 human_years = int(input("Input a dog's age in human years: "))
-print(human_years)
+
+if human_years <= 2:
+    dog_years = human_years * 10
+else:
+    dog_years = 20 + ((human_years - 2) * 7)
+
+print(f"The dog's age in dog years is {dog_years}")
